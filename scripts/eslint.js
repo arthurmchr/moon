b5152e81861e144c30eb8ceb6007a9cff6cb82dd
Arthur Muchir <arthur.muchir@gmail.com>
2016-06-10 14:38:20 +0200

    rmv log

--- scripts/eslint.js
+++ scripts/eslint.js
@@ -4,7 +4,7 @@ const CLIEngine = require('eslint').CLIEngine;
 
 const cli = new CLIEngine();
 const formatter = cli.getFormatter();
-console.log(path.join(__dirname, '../app/media/js/**/*.js'), path.join(__dirname, '../silex/public/media/js/bundle.js'));
+
 chokidar.watch(path.join(__dirname, '../app/media/js/**/*.js'), {
 	ignored: [
 		path.join(__dirname, '../app/media/js/bundle.js'),
