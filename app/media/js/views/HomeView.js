33475577c7949ee59bf1d2d37bc601790702e77e
Arthur Muchir <arthur.muchir@gmail.com>
2016-10-04 17:54:13 +0200

    update libs

--- app/media/js/views/HomeView.js
+++ app/media/js/views/HomeView.js
@@ -4,7 +4,7 @@ export default class HomeView extends AbstractView {
 
 	static get className() {
 
-		return __filename.match(new RegExp(/.*\/([a-z]+)\.js$/i))[1];
+		return __filename.match(new RegExp(/.*\/([A-z]+)\.js$/))[1];
 	}
 
 	constructor() {
